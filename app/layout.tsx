9be1a878312017f1923fa3c1a1f08b852e4467d2
jyefuriii <jeffrey.t.fabella@gmail.com>
2024-11-25 02:48:41 +0800

    Settings.json files exclude

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "DevFlow",
   description:
         "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  icons:{icon:'/image/site-logo.svg'}
+  icons:{icon:'/images/site-logo.svg'}
 };
 
 export default function RootLayout({
