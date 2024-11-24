5556ee14014a3c8c8e96f6991e01daea3fb09005
jyefuriii <jeffrey.t.fabella@gmail.com>
2024-11-24 23:53:18 +0800

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-function Home() {
+const Home = () => {
   return (
-    <h1 className="text-3xl text-white-700 font-black">
+    <h1 className="text-3xl font-black text-white">
       Welcome to the world of Next.js
     </h1>
   );
-}
+};
 export default Home;
