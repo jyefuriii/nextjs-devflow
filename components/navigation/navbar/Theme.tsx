c0b95ed1dc75293cc6f30e194029ae7f268b4a17
jyefuriii <jeffrey.t.fabella@gmail.com>
2024-11-26 03:59:45 +0800

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and Github Providers

--- components/navigation/navbar/Theme.tsx
+++ components/navigation/navbar/Theme.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,34 +14,40 @@ import {
 
 function Theme() {
   const { setTheme, resolvedTheme: currentTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Ensure the component only renders on the client
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Prevent rendering on the server
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild suppressHydrationWarning>
+      <DropdownMenuTrigger asChild>
         <Button
           size="icon"
-          variant={"outline"}
+          variant="outline"
           className="cursor-pointer outline-none focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200"
         >
           {currentTheme === "light" && (
             <Image
-              src={"/icons/sun.svg"}
+              src="/icons/sun.svg"
               width={30}
               height={30}
               alt="light mode logo"
               priority
-              loading="eager"
               className="active-theme size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
             />
           )}
           {currentTheme === "dark" && (
             <Image
-              src={"/icons/moon.svg"}
+              src="/icons/moon.svg"
               width={30}
               height={30}
               alt="dark mode logo"
               priority
-              loading="eager"
               className="active-theme absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
             />
           )}
@@ -50,35 +57,32 @@ function Theme() {
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           <Image
-            src={"/icons/sun.svg"}
+            src="/icons/sun.svg"
             width={20}
             height={20}
             alt="light mode logo"
             priority
-            loading="eager"
             className="invert-colors"
           />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           <Image
-            src={"icons/moon.svg"}
+            src="/icons/moon.svg"
             width={20}
             height={20}
-            alt="light mode logo"
+            alt="dark mode logo"
             priority
-            loading="eager"
           />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           <Image
-            src={"/icons/computer.svg"}
+            src="/icons/computer.svg"
             width={20}
             height={20}
-            alt="light mode logo"
+            alt="system mode logo"
             priority
-            loading="eager"
           />
           System
         </DropdownMenuItem>
